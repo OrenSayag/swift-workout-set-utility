2e97427b3ddaad974fb5754429eb81d3ad442390
Oren Sayag <oren.s@techmarketing.co.il>
2024-02-10 17:22:30 +0200

    Initial Commit

---
+++ workout-stopwatch/workout_stopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  workout_stopwatchApp.swift
+//  workout-stopwatch
+//
+//  Created by Oren Sayag on 10/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct workout_stopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
